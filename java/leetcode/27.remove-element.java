da9f69903ae52c3c3dd87c8f870a0f9e3438c423
vczk <vczk@msn.com>
2023-01-05 23:21:49 +0800

    add leetcode 27

---
+++ java/leetcode/27.remove-element.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=27 lang=java
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int fast = 0;
+        int slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
+// @lc code=end
