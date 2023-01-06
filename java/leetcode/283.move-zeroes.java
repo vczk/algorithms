1e5cc3a881747f5592a320ec1e25731c5455b459
vczk <vczk@msn.com>
2023-01-06 22:39:23 +0800

    add leetcode 283

---
+++ java/leetcode/283.move-zeroes.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=283 lang=java
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int fast = 0;
+        int slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        while (slow < nums.length) {
+            nums[slow] = 0;
+            slow++;
+        }
+    }
+}
+// @lc code=end
